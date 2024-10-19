377f58e432250266000b80bf29d488e5b1b89ce6
unknown <raafilsamad122004@gmail.com>
2024-10-19 16:36:30 +0530

    add python file

---
+++ inheritance.py
@@ -0,0 +1,16 @@
+class grandpa():
+    def diary(self):
+        print("GRANDPA'S DAIRY")
+class dad(grandpa):
+    def car(self):
+        print("DAD'S CAR")
+class daughter(dad):
+    def phone(self):
+        print("DAUGHTER'S PHONE")
+raafil=daughter()
+raafil.phone()
+raafil.car()
+raafil.diary()
+        
+
+    
